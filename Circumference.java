ec9205e29371b8e6ffde243d5d34940cc20dc117
tammyw25 <tammy.wong25@ydsbk12.ca>
2024-02-27 16:27:10 -0500

    updated circumferecne

--- Circumference.java
+++ Circumference.java
@@ -8,6 +8,10 @@ class Circumference extends ConsoleProgram {
   public void run() {
     
     // start coding here
-    
+    double dblRadius;
+    double dblCircumference;
+    dblRadius = readDouble("Enter radius: ");
+    dblCircumference = (2 * 3.14 * dblRadius);
+    System.out.println("The circumference of this circle is: " + dblCircumference);
   }
 }
